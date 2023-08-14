c4003f6d576711695638604a51f59f6f004916df
ishaf <shafiulislam20@gmail.com>
2023-08-14 19:28:12 +0600

    fix(file): get file read stream

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,33 +1,44 @@
-import { Body, Controller, Delete, Get, Param, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
-import { FileService } from "./file.service";
-import { FileInterceptor } from "@nestjs/platform-express";
-import { CreateFileDto } from "./dto/create-file.dto";
-import { ApiAcceptedResponse, ApiConsumes, ApiTags } from "@nestjs/swagger";
-import { UploadFileEntity } from "./entities/file.entity";
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    UploadedFile,
+    UseInterceptors,
+    Response
+} from "@nestjs/common";
+import {FileService} from "./file.service";
+import {FileInterceptor} from "@nestjs/platform-express";
+import {CreateFileDto} from "./dto/create-file.dto";
+import {ApiAcceptedResponse, ApiConsumes, ApiTags} from "@nestjs/swagger";
+import {UploadFileEntity} from "./entities/file.entity";
 import * as stream from "stream";
 
 @ApiTags("File")
 @Controller("file")
 export class FileController {
-  constructor(private readonly fileService: FileService) {
-  }
+    constructor(private readonly fileService: FileService) {
+    }
 
-  @Post()
-  @ApiConsumes("multipart/form-data")
-  @UseInterceptors(FileInterceptor("file"))
-  @ApiAcceptedResponse({ type: UploadFileEntity })
-  upload(@Body() data: CreateFileDto, @UploadedFile() file: Express.Multer.File) {
-    return this.fileService.upload(file);
-  }
+    @Post()
+    @ApiConsumes("multipart/form-data")
+    @UseInterceptors(FileInterceptor("file"))
+    @ApiAcceptedResponse({type: UploadFileEntity})
+    upload(@Body() data: CreateFileDto, @UploadedFile() file: Express.Multer.File) {
+        return this.fileService.upload(file);
+    }
 
-  @Get(":key")
-  @ApiAcceptedResponse({ type: stream.Readable })
-  findOne(@Param("key") key: string) {
-    return this.fileService.findOne(key);
-  }
+    @Get(":key")
+    @ApiAcceptedResponse({type: stream.Readable})
+    async findOne(@Param("key") key: string, @Response() res,) {
+        const gg = await this.fileService.findOne(key);
+        gg.pipe(res)
+    }
 
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.fileService.remove(+id);
-  }
+    @Delete(":id")
+    remove(@Param("id") id: string) {
+        return this.fileService.remove(+id);
+    }
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,70 +1,63 @@
-import { Injectable, StreamableFile } from "@nestjs/common";
+import {Injectable} from "@nestjs/common";
 import * as process from "process";
 import * as AWS from "aws-sdk";
-import { S3 } from "aws-sdk";
-import { UploadFileEntity } from "./entities/file.entity";
+import {S3} from "aws-sdk";
+import {UploadFileEntity} from "./entities/file.entity";
 
 @Injectable()
 export class FileService {
-  #s3: S3;
+    #s3: S3;
 
-  constructor() {
-    this.#s3 = new AWS.S3({
-      endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
-      credentials: {
-        accessKeyId: `${process.env.R2_ACCESS_KEY_ID}`,
-        secretAccessKey: `${process.env.R2_ACCESS_KEY_SECRET}`
-      },
-      signatureVersion: "v4"
-    });
-  }
-
-  #generateString(length) {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let result = " ";
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    constructor() {
+        this.#s3 = new AWS.S3({
+            endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+            credentials: {
+                accessKeyId: `${process.env.R2_ACCESS_KEY_ID}`,
+                secretAccessKey: `${process.env.R2_ACCESS_KEY_SECRET}`
+            },
+            signatureVersion: "v4"
+        });
     }
 
-    return result;
-  }
+    #generateString(length) {
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let result = " ";
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
 
-  async upload(file: Express.Multer.File): Promise<UploadFileEntity> {
-    const key = `${this.#generateString(10).replace(" ", "")}${file.originalname.replace(" ", "").toLowerCase()}`;
-    console.log();
-    const uploadParams = {
-      Bucket: process.env.R2_BUCKET,
-      Body: file.buffer,
-      Key: key
-    };
+        return result;
+    }
 
-    await this.#s3.putObject(uploadParams, (err, data) => {
-      console.log(err);
-      console.log(data);
-    });
-    return {
-      key
-    };
-  }
+    async upload(file: Express.Multer.File): Promise<UploadFileEntity> {
+        const key = `${this.#generateString(10).replace(" ", "")}${file.originalname.replace(" ", "").toLowerCase()}`;
+        console.log();
+        const uploadParams = {
+            Bucket: process.env.R2_BUCKET,
+            Body: file.buffer,
+            Key: key
+        };
 
-  async findOne(id: string) {
-    console.log(id);
-    const downloadParams = {
-      Key: id,
-      Bucket: process.env.R2_BUCKET
-    };
-    let res;
-    res = await this.#s3.getObject(downloadParams, (err, data) => {
-      return {
-        data,
-        err
-      };
-    });
-    return new StreamableFile(res.createReadStream());
-  }
+        await this.#s3.putObject(uploadParams, (err, data) => {
+            console.log(err);
+            console.log(data);
+        });
+        return {
+            key
+        };
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} file`;
-  }
+    async findOne(id: string) {
+        console.log(id);
+        const downloadParams = {
+            Key: id,
+            Bucket: process.env.R2_BUCKET
+        };
+        return this.#s3.getObject(downloadParams).createReadStream();
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} file`;
+    }
 }
