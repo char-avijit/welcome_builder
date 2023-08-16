8ae8a5e2d72cdc1c836c1d3ad6ce9eecd0aa261e
ishaf <shafiulislam20@gmail.com>
2023-08-16 23:38:41 +0600

    chore(prisma): migration files

--- prisma/migrations/20230520191756_testimonials/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Testimonials" (
-    "id" SERIAL NOT NULL,
-    "message" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "designation" TEXT NOT NULL,
-
-    CONSTRAINT "Testimonials_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20230521115013_unic_name/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name]` on the table `Testimonials` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Testimonials_name_key" ON "Testimonials"("name");
