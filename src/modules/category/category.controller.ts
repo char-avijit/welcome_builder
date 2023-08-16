7602133c3cfc023f58abe06f01a9b03e8d10b7c7
ishaf <shafiulislam20@gmail.com>
2023-08-16 21:44:48 +0600

    feat(category): get category, update and delete

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,45 +1,59 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query} from '@nestjs/common';
-import {CategoryService} from './category.service';
-import {CreateCategoryDto} from './dto/create-category.dto';
-import {UpdateCategoryDto} from './dto/update-category.dto';
-import {ApiBearerAuth, ApiCreatedResponse, ApiDefaultResponse, ApiTags} from "@nestjs/swagger";
-import {JwtAuthGuard} from "../auth/jwt-auth.guard";
-import {CategoryEntity} from "./entities/category.entity";
-import {CategoriesEntity} from "./entities/categories.entity";
-import {FiltersDto} from "../../common/dto/filters.dto";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from "@nestjs/common";
+import { CategoryService } from "./category.service";
+import { CreateCategoryDto } from "./dto/create-category.dto";
+import { UpdateCategoryDto } from "./dto/update-category.dto";
+import {
+  ApiAcceptedResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiDefaultResponse,
+  ApiNoContentResponse,
+  ApiTags
+} from "@nestjs/swagger";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import { CategoryEntity } from "./entities/category.entity";
+import { CategoriesEntity } from "./entities/categories.entity";
+import { FiltersDto } from "../../common/dto/filters.dto";
 
 @ApiTags("Category")
-@Controller('category')
+@Controller("category")
 export class CategoryController {
-    constructor(private readonly categoryService: CategoryService) {
-    }
+  constructor(private readonly categoryService: CategoryService) {
+  }
 
-    @Post()
-    @ApiBearerAuth()
-    @UseGuards(JwtAuthGuard)
-    @ApiCreatedResponse({type: CategoryEntity})
-    create(@Body() createCategoryDto: CreateCategoryDto) {
-        return this.categoryService.create(createCategoryDto);
-    }
+  @Post()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @ApiCreatedResponse({ type: CategoryEntity })
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoryService.create(createCategoryDto);
+  }
 
-    @Get()
-    @ApiDefaultResponse({type: CategoriesEntity})
-    findAll(@Query() filtersDto?: FiltersDto) {
-        return this.categoryService.findAll(filtersDto);
-    }
+  @Get()
+  @ApiDefaultResponse({ type: CategoriesEntity })
+  findAll(@Query() filtersDto?: FiltersDto) {
+    return this.categoryService.findAll(filtersDto);
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.categoryService.findOne(+id);
-    }
+  @Get(":id")
+  @ApiDefaultResponse({ type: CategoryEntity })
+  findOne(@Param("id") id: string) {
+    return this.categoryService.findOne(+id);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-        return this.categoryService.update(+id, updateCategoryDto);
-    }
+  @Patch(":id")
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @ApiAcceptedResponse({ type: CategoryEntity })
+  update(@Param("id") id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+    return this.categoryService.update(+id, updateCategoryDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.categoryService.remove(+id);
-    }
+  @Delete(":id")
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @ApiNoContentResponse({ type: CategoryEntity })
+  remove(@Param("id") id: string) {
+    return this.categoryService.remove(+id);
+  }
 }
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,46 +1,59 @@
-import {Injectable} from '@nestjs/common';
-import {CreateCategoryDto} from './dto/create-category.dto';
-import {UpdateCategoryDto} from './dto/update-category.dto';
-import {PrismaService} from "../../common/prisma/prisma.service";
-import {FiltersDto} from "../../common/dto/filters.dto";
-import {CategoriesEntity} from "./entities/categories.entity";
+import { Injectable } from "@nestjs/common";
+import { CreateCategoryDto } from "./dto/create-category.dto";
+import { UpdateCategoryDto } from "./dto/update-category.dto";
+import { PrismaService } from "../../common/prisma/prisma.service";
+import { FiltersDto } from "../../common/dto/filters.dto";
+import { CategoriesEntity } from "./entities/categories.entity";
 
 @Injectable()
 export class CategoryService {
-    constructor(private prisma: PrismaService) {
-    }
+  constructor(private prisma: PrismaService) {
+  }
 
-    create(createCategoryDto: CreateCategoryDto) {
-        return this.prisma.showCaseCategory.create({data: createCategoryDto});
-    }
+  create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.showCaseCategory.create({ data: createCategoryDto });
+  }
 
-    async findAll(filtersDto?: FiltersDto): Promise<CategoriesEntity> {
-        let pageNo = filtersDto.pageNo ?? 1
-        if (pageNo == 0) {
-            pageNo = 1
-        }
-        const limitPerPage = filtersDto.limitPerPage ?? 10
-        const skip = (pageNo - 1) * limitPerPage
-        const data = await this.prisma.showCaseCategory.findMany({
-            skip,
-            take: parseInt(String(limitPerPage))
-        })
-        const count = await this.prisma.showCaseCategory.count()
-        return ({
-            count,
-            results: data
-        })
+  async findAll(filtersDto?: FiltersDto): Promise<CategoriesEntity> {
+    let pageNo = filtersDto.pageNo ?? 1;
+    if (pageNo == 0) {
+      pageNo = 1;
     }
+    const limitPerPage = filtersDto.limitPerPage ?? 10;
+    const skip = (pageNo - 1) * limitPerPage;
+    const data = await this.prisma.showCaseCategory.findMany({
+      skip,
+      take: parseInt(String(limitPerPage))
+    });
+    const count = await this.prisma.showCaseCategory.count();
+    return ({
+      count,
+      results: data
+    });
+  }
 
-    findOne(id: number) {
-        return `This action returns a #${id} category`;
-    }
+  async findOne(id: number) {
+    return this.prisma.showCaseCategory.findFirst({
+      where: {
+        id
+      }
+    });
+  }
 
-    update(id: number, updateCategoryDto: UpdateCategoryDto) {
-        return `This action updates a #${id} category`;
-    }
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return this.prisma.showCaseCategory.update({
+      data: updateCategoryDto,
+      where: {
+        id
+      }
+    });
+  }
 
-    remove(id: number) {
-        return `This action removes a #${id} category`;
-    }
+  remove(id: number) {
+    return this.prisma.showCaseCategory.delete({
+      where: {
+        id
+      }
+    });
+  }
 }
