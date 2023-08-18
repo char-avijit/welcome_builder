e7c3eddf4418a7fa74fd4314f9e8beb00439e865
ishaf <shafiulislam20@gmail.com>
2023-08-19 01:13:04 +0600

    chore(seed): seed for new db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,66 +1,73 @@
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 import * as process from "process";
+import { AreaUnit, Currency, PropertyType } from "../src/common/helper/enum";
 // initialize Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
-    // Testimonial
-    await prisma.testimonials.upsert({
-        create: {
-            message: 'sdfsdfsdf',
-            designation: 'sdfsdfs',
-            name: 'ishaf'
-        }, update: {}, where: {name: 'ishaf'}
+  // Testimonial
+  await prisma.testimonials.upsert({
+    create: {
+      message: "sdfsdfsdf",
+      designation: "sdfsdfs",
+      name: "ishaf",
+      avatar: "sdfsdfsd"
+    }, update: {}, where: { name: "ishaf" }
 
-    });
+  });
 
-    await prisma.testimonials.upsert({
-        create: {
-            message: 'ami shundor comment korsi ekta like diya jan ',
-            designation: 'ami bekar',
-            name: 'ami ekta user'
-        }, update: {}, where: {name: 'ami ekta user'}
-    });
+  await prisma.testimonials.upsert({
+    create: {
+      message: "ami shundor comment korsi ekta like diya jan ",
+      designation: "ami bekar",
+      name: "ami ekta user",
+      avatar: "sdfsdfsd"
+    }, update: {}, where: { name: "ami ekta user" }
+  });
 
-    await prisma.showCaseCategory.upsert({
-        create: {
-            name: "category one",
-            description: 'this is a description',
-        }, update: {}, where: {id: 1}
-    });
-    await prisma.showCaseCategory.upsert({
-        create: {
-            name: "category 2",
-            description: 'this is a description',
-        }, update: {}, where: {id: 1}
-    });
-    await prisma.showCase.upsert({
-        create: {
-            name: 'dsdfsd',
-            description: 'dfsdfsdf',
-            type: 'dd',
-            address: 'ddd',
-            CategoryId: 1,
-            slug: 'dddddddddd'
-        }, update: {}, where: {id: 1}
-    });
+  await prisma.showCaseCategory.upsert({
+    create: {
+      name: "category one",
+      description: "this is a description"
+    }, update: {}, where: { id: 1 }
+  });
+  await prisma.showCaseCategory.upsert({
+    create: {
+      name: "category 2",
+      description: "this is a description"
+    }, update: {}, where: { id: 1 }
+  });
+  await prisma.showCase.upsert({
+    create: {
+      name: "dsdfsd",
+      description: "dfsdfsdf",
+      type: PropertyType.ROOM,
+      address: "ddd",
+      CategoryId: 1,
+      slug: "dddddddddd",
+      area: 1244,
+      price: 144454,
+      areaUnit: AreaUnit.SQUARE_METERS,
+      currency: Currency.USD
+    }, update: {}, where: { id: 1 }
+  });
 
-    await prisma.showCaseImages.upsert({
-        create: {
-            showCaseId: 1,
-            key: 'test.png'
-        }, update: {}, where: {id: 1}
-    });
+  await prisma.showCaseImages.upsert({
+    create: {
+      showCaseId: 1,
+      key: "test.png"
+    }, update: {}, where: { id: 1 }
+  });
 
 }
 
 // execute the main function
 main()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        // close Prisma Client at the end
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
