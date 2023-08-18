d38575f477f5c05bdba2a45745fccc02bed08d15
ishaf <shafiulislam20@gmail.com>
2023-08-18 20:43:35 +0600

    fix(CORS): enable for all

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: ["*"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true
+  });
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
-    .setTitle('Welcome Build')
-    .setDescription('Welcome Build backend api docs')
-    .setVersion('0.1')
+    .setTitle("Welcome Build")
+    .setDescription("Welcome Build backend api docs")
+    .setVersion("0.1")
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup("docs", app, document);
 
   await app.listen(3000);
 }
