9d71166d97bfe204f0caf76cae6303db905a8c44
ishaf <shafiulislam20@gmail.com>
2023-08-24 19:35:50 +0600

    feat: logger

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { ValidationPipe } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { Logger as LoggerClass, ValidationPipe } from "@nestjs/common";
+import * as process from "process";
+import * as helmet from "helmet";
+import { greenBright } from "cli-color";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
+const logger = new LoggerClass("Bootstrap");
+const PORT = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 3000;
+(async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(helmet.default());
+  app.enableCors({ origin: true });
+
+  /*app.enableCors({
     origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
-  });
+  });*/
+
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle("Welcome Build")
@@ -21,7 +31,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs", app, document);
 
-  await app.listen(3000);
-}
+  await app.listen(PORT);
 
-bootstrap();
+  logger.log(`🚀🚀 Started Server 🚀🚀 at PORT ${greenBright(PORT)}`);
+  logger.log(`http://localhost:${PORT}`);
+})();
