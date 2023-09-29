173913b343114544587015db77c0ed4ced179502
ishaf <shafiulislam20@gmail.com>
2023-09-29 10:40:40 +0600

    testsd 456aadads

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,8 @@
 import { Injectable } from "@nestjs/common";
-/* eslint-disable no-console */
-var figlet = require("figlet");
 
 @Injectable()
 export class AppService {
   async getHello(): Promise<any> {
-    let figletData: any = "";
-    await figlet("ishaf", {
-      font: "Doh"
-    }, (err: any, data: any) => {
-      figletData = data;
-    });
-    if (figletData !='') {
-      return figletData;
-    }
 
   }
 }
